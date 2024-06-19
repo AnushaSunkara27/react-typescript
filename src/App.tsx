192d881f91d79946dfde1105aa49b8d825271803
AnushaSunkara27 <anusha.vjcf1@gmail.com>
2024-06-19 14:49:13 -0500

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let name: string = 'Anusha';
+let age: number = 20;
+let isStudent: boolean;
+let hobbies: string[];
+let role: [number, string];
+
+let personName: any; // any type
+let personAge: unknown;
+
+type Person = {
+  name: string;
+  age?: number;   // optional 
+}
+
+let person: Person = {
+  name: 'anusha',
+  age: 5
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello
     </div>
   );
 }
